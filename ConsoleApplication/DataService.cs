fc6b61cde3c51440795c9c01b42296e61f9e57d7
Aline Borges <alinekborges@gmail.com>
2014-05-20 20:10:31 +1000

    All with some css and API

---
+++ ConsoleApplication/DataService.cs
@@ -0,0 +1,244 @@
+﻿using Data.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public static class DataService
+    {
+        public static string filename = "stations.txt";
+        public static string result;
+        public static bool error;
+
+        public static bool ready = false;
+
+        public static string path = @"C:\Users\Aline Borges\StationList.txt";
+
+        public static List<StationModel> stationListInBackground;
+
+        public static string networkUrl = "http://api.citybik.es/v2/networks";
+
+        public static List<string> allStations = new List<string>();
+
+        public static int NetworkSize;
+
+        public static void GetStationList(string urlToCall)
+        {
+
+                WebClient client = new WebClient();
+                client.DownloadStringCompleted += client_DownloadStringCompleted;
+                client.DownloadStringAsync(new Uri(urlToCall));
+        }
+
+        public static void GetStationList(NetworkModel network)
+        {
+            
+            string urlToCall = "http://api.citybik.es/v2/networks/" + network.id;
+
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_DownloadStringCompleted;
+            client.DownloadStringAsync(new Uri(urlToCall));
+
+        }
+
+        public static void GetNetworkListFromFile()
+        {
+            string result = System.IO.File.ReadAllText(path);
+
+            try
+            {
+                List<NetworkModel> networkList = JsonConvert.DeserializeObject<List<NetworkModel>>(result);
+                foreach (NetworkModel network in networkList)
+                {
+                    Console.WriteLine("Network: " + network.Name + "   Stations count: " + network.Stations.Count);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+        }
+
+
+        private static void client_DownloadStringCompletedInBackground(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                result = e.Result;
+                Debug.WriteLine("reading from API...");
+                Debug.WriteLine(result);
+                //saveText(filename, result);
+                stationListInBackground = JsonConvert.DeserializeObject<List<StationModel>>(result);
+
+            }
+            else
+            {
+                Debug.WriteLine("ERROR downloading api");
+                error = true;
+              
+            }
+        }
+
+        public static void GetNetworkList()
+        {
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_DownloadStringNetworkCompleted;
+            client.DownloadStringAsync(new Uri(networkUrl));
+        }
+
+
+        private static void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                string result = e.Result;
+                allStations.Add(result);
+                result.Insert(result.Length - 2, ",");
+                System.IO.File.AppendAllText(DataService.path, result);
+                
+                Console.WriteLine("reading from API... " + NetworkSize);
+                NetworkSize--;
+                //Console.WriteLine(result);
+                //saveText(filename, result);
+                //List<StationModel> stationList = JsonConvert.DeserializeObject<List<StationModel>>(result);
+
+                NetworkModel network = JsonConvert.DeserializeObject<NetworkModel>(result);
+
+                List<StationModel> stationList = network.Stations;
+
+                if (stationList != null)
+                {
+                    List<StationModel> stations = new List<StationModel>(stationList);
+
+                    Console.WriteLine("station count = " + stations.Count);
+                    
+
+                    //allStations.Add(result);
+                                    
+                }
+
+                /*
+                    if (NetworkSize <= 0)
+                    {
+                        Console.WriteLine("I think I'm the last one");
+                        Console.WriteLine("saving to file " + DataService.allStations.Count);
+
+                        foreach (string text in DataService.allStations)
+                        {
+                            Console.WriteLine("saving network");
+                            System.IO.File.AppendAllText(DataService.path, text);
+                        }
+                    }
+                 */
+                
+            }
+            else
+            {
+                Debug.WriteLine("ERROR downloading api of stations ");
+                Console.WriteLine("ERROR downloading api of stations ");
+                
+            }
+        }
+
+        private static void client_DownloadStringNetworkCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                result = e.Result;
+                Console.WriteLine("reading networks......");
+                Debug.WriteLine(result);
+                //saveText(filename, result);
+
+                    try {
+                        //List<NetworkModel> networkList = JsonConvert.DeserializeObject<NetworkModel[]>(result);
+                        //NetworkModel[] networkList = JsonConvert.DeserializeObject<NetworkModel[]>(result);
+                        AllNetworks networks = JsonConvert.DeserializeObject<AllNetworks>(result);
+
+                        List<NetworkModel> networkList = networks.Networks;
+
+                        if (networkList != null)
+                        {
+
+                            foreach (NetworkModel network in networkList)
+                            {
+                                Console.WriteLine("Network: " + network.Name);
+                                GetStationList(network);
+                                //System.IO.File.AppendAllText(path, result);
+
+                            }
+                            NetworkSize = networkList.Count;
+                            Console.WriteLine("network size = " + networkList.Count);
+
+
+                        }
+                    } catch (Exception ex) {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error downloading network information. Check your internet connection");
+                    
+                }
+            
+        }
+
+        /*
+        private static void saveText(string filename, string text)
+        {
+            using (IsolatedStorageFile isf =
+                          IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                using (IsolatedStorageFileStream rawStream =
+                                 isf.CreateFile(filename))
+                {
+                    StreamWriter writer = new StreamWriter(rawStream);
+                    writer.Write(text);
+                    writer.Close();
+                }
+            }
+        }
+         */
+
+        /*
+        private static bool loadText(string filename, out string result)
+        {
+            result = "";
+            using (IsolatedStorageFile isf =
+                IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (isf.FileExists(filename))
+                {
+                    try
+                    {
+                        using (IsolatedStorageFileStream rawStream =
+                            isf.OpenFile(filename, System.IO.FileMode.Open))
+                        {
+                            StreamReader reader = new StreamReader(rawStream);
+                            result = reader.ReadToEnd();
+                            reader.Close();
+                        }
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+         */
+    }
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService.GetNetworkListFromFile();
+            //DataService.GetNetworkList();
+            //while (DataService.ready == false) ;
+            
+            Console.Read();
+            
+        }
+    }
+}
